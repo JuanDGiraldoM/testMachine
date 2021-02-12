5d8c9c14bf50e491a898cab7ed8dd1b4b4136221
Juan D Giraldo M <juandagiraldoma@gmail.com>
2021-02-12 08:56:56 -0500

    Update test script

--- test.sh
+++ test.sh
@@ -1,56 +1,54 @@
 # !/bin/sh
 set -e
 
-function catchError() {
-    echo -e "\e[31mIt has failed to $1!\e[0m"
-}
-
 echo
 echo =============================================================
 echo Git
 echo =============================================================
-git --version || catchError "open git"
+git --version
 
 echo
 echo =============================================================
 echo AWS CLI
 echo -e "\e[33mit should be version 2!\e[0m"
 echo =============================================================
-aws --version || catchError "open aws cli"
+aws --version
 
 echo
 echo =============================================================
 echo Docker
 echo =============================================================
-docker -v || catchError "open docker"
+docker -v
+docker image ls >logs/docker-images.log
 
 echo
 echo =============================================================
 echo Kubectl
 echo =============================================================
-kubectl version --client || catchError "open k8s"
+kubectl version --client
+aws eks update-kubeconfig --name eks-ecosistemas-de-riesgos-qa
 
 echo
 echo =============================================================
 echo Java
 echo -e "\e[33mit should be 11!\e[0m"
 echo =============================================================
-java --version || catchError "open java"
+java --version
 
 echo
 echo =============================================================
 echo Gradle
 echo =============================================================
-gradle -v || catchError "open gradle"
+gradle -v
 
 echo
 echo =============================================================
 echo npm
 echo =============================================================
-npm -v || catchError "open npm"
+npm -v
 
 echo
 echo =============================================================
 echo ng
 echo =============================================================
-ng version || catchError "open ng"
+ng version
